c94a846a579502c84d59acb34f8e05f343169e22
Renee Li <renee.li@fivetran.com>
2024-04-03 17:15:45 -0400

    sep the count distinct condition to make redshift happy

--- models/servicenow__user_aggregated.sql
+++ models/servicenow__user_aggregated.sql
@@ -30,31 +30,16 @@ user_group as (
     from {{ ref('stg_servicenow__sys_user_group')}}
 ),
 
-user_aggregates as (
+aggregate_counts as (
 
-    select
+    select 
         sys_user.sys_user_id,
         sys_user.source_relation,
-        case when
-            count(user_grmember.sys_user_group_id) <= 5000 then {{ fivetran_utils.string_agg("distinct user_grmember.sys_user_group_id", "'\\n'") }}
-            else 'Too many sys user group ids to render' 
-        end as sys_user_group_ids,
-        case when
-            count(user_has_role.sys_user_role_id) <= 5000 then {{ fivetran_utils.string_agg("distinct user_has_role.sys_user_role_id", "'\\n'") }}
-            else 'Too many sys user role ids to render' 
-        end as sys_user_role_ids,
-        case when
-            count(user_role.sys_user_role_name) <= 5000 then {{ fivetran_utils.string_agg("distinct user_role.sys_user_role_name", "'\\n'") }}
-            else 'Too many sys user role names to render' 
-        end as sys_user_role_names,
-        case when
-            count(user_role.includes_roles) <= 5000 then {{ fivetran_utils.string_agg("distinct user_role.includes_roles", "'\\n'") }}
-            else 'Too many roles to render' 
-        end as included_roles,
-        case when
-            count(user_group.sys_user_group_roles) <= 5000 then {{ fivetran_utils.string_agg("distinct user_group.sys_user_group_roles", "'\\n'") }}
-            else 'Too many sys user group roles to render' 
-        end as sys_user_group_roles
+        count(distinct user_grmember.sys_user_group_id) as count_distinct_sys_user_group_ids,
+        count(distinct user_has_role.sys_user_role_id) as count_distinct_sys_user_role_ids,
+        count(distinct user_role.sys_user_role_name) as count_distinct_sys_user_role_names,
+        count(distinct user_role.includes_roles) as count_distinct_included_roles,
+        count(distinct user_group.sys_user_group_roles) as count_distinct_sys_user_group_roles
 
     from sys_user
     left join user_grmember
@@ -71,6 +56,52 @@ user_aggregates as (
         and user_has_role.source_relation = user_role.source_relation
 
     {{ dbt_utils.group_by(2) }}
+),
+
+user_aggregates as (
+
+    select
+        sys_user.sys_user_id,
+        sys_user.source_relation,
+        case when
+            aggregate_counts.count_distinct_sys_user_group_ids <= 5000 then {{ fivetran_utils.string_agg("distinct user_grmember.sys_user_group_id", "'\\n'") }}
+            else 'Too many sys user group ids to render' 
+        end as sys_user_group_ids,
+        case when
+            aggregate_counts.count_distinct_sys_user_role_ids <= 5000 then {{ fivetran_utils.string_agg("distinct user_has_role.sys_user_role_id", "'\\n'") }}
+            else 'Too many sys user role ids to render' 
+        end as sys_user_role_ids,
+        case when
+            aggregate_counts.count_distinct_sys_user_role_names <= 5000 then {{ fivetran_utils.string_agg("distinct user_role.sys_user_role_name", "'\\n'") }}
+            else 'Too many sys user role names to render' 
+        end as sys_user_role_names,
+        case when
+            aggregate_counts.count_distinct_included_roles <= 5000 then {{ fivetran_utils.string_agg("distinct user_role.includes_roles", "'\\n'") }}
+            else 'Too many roles to render' 
+        end as included_roles,
+        case when
+            aggregate_counts.count_distinct_sys_user_group_roles <= 5000 then {{ fivetran_utils.string_agg("distinct user_group.sys_user_group_roles", "'\\n'") }}
+            else 'Too many sys user group roles to render' 
+        end as sys_user_group_roles
+
+    from sys_user
+    left join aggregate_counts
+        on sys_user.sys_user_id = aggregate_counts.sys_user_id
+        and sys_user.source_relation = aggregate_counts.source_relation
+    left join user_grmember
+        on sys_user.sys_user_id = user_grmember.sys_user_id
+        and sys_user.source_relation = user_grmember.source_relation
+    left join user_group
+        on user_grmember.sys_user_group_id = user_group.sys_user_group_id
+        and user_grmember.source_relation = user_group.source_relation
+    left join user_has_role
+        on sys_user.sys_user_id = user_has_role.sys_user_id
+        and sys_user.source_relation = user_has_role.source_relation
+    left join user_role
+        on user_has_role.sys_user_role_id = user_role.sys_user_role_id
+        and user_has_role.source_relation = user_role.source_relation
+
+    group by sys_user_id, source_relation, count_distinct_sys_user_group_ids, count_distinct_sys_user_role_ids, count_distinct_sys_user_role_names, count_distinct_included_roles, count_distinct_sys_user_group_roles
 )
 
 select * 
