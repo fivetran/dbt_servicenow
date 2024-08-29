fe66067a7ffbbd9739823bf2144d6929e70056c4
Renee Li <renee.li@fivetran.com>
2024-08-28 23:39:24 -0400

    update user aggregated aggs test

--- integration_tests/tests/consistency/user_aggregated_aggs.sql
+++ integration_tests/tests/consistency/user_aggregated_aggs.sql
@@ -13,7 +13,6 @@ with prod as (
         count_distinct_included_roles,
         count_distinct_sys_user_group_roles
     from {{ target.schema }}_servicenow_prod.servicenow__user_aggregated
-    group by 1,2
 ),
 
 dev as (
@@ -27,25 +26,24 @@ dev as (
         count_distinct_sys_user_group_roles
 
     from {{ target.schema }}_servicenow_dev.servicenow__user_aggregated
-    group by 1,2
 ),
 
 final as (
     select
-        prod.sys_user_id,
-        dev.sys_user_id,
-        prod.source_relation,
-        dev.source_relation,
-        prod.count_distinct_sys_user_group_ids,
-        dev.count_distinct_sys_user_group_ids,
-        prod.count_distinct_sys_user_role_ids,
-        dev.count_distinct_sys_user_role_ids,
-        prod.count_distinct_sys_user_role_names,
-        dev.count_distinct_sys_user_role_names,
-        prod.count_distinct_included_roles,
-        dev.count_distinct_included_roles,
-        prod.count_distinct_sys_user_group_roles,
-        dev.count_distinct_sys_user_group_roles
+        prod.sys_user_id as prod_sys_user_id,
+        dev.sys_user_id as dev_sys_user_id,
+        prod.source_relation as prod_source_relation,
+        dev.source_relation as dev_source_relation,
+        prod.count_distinct_sys_user_group_ids as prod_count_distinct_sys_user_group_ids,
+        dev.count_distinct_sys_user_group_ids as dev_count_distinct_sys_user_group_ids,
+        prod.count_distinct_sys_user_role_ids as prod_count_distinct_sys_user_role_ids,
+        dev.count_distinct_sys_user_role_ids as dev_count_distinct_sys_user_role_ids,
+        prod.count_distinct_sys_user_role_names as prod_count_distinct_sys_user_role_names,
+        dev.count_distinct_sys_user_role_names as dev_count_distinct_sys_user_role_names,
+        prod.count_distinct_included_roles as prod_count_distinct_included_roles,
+        dev.count_distinct_included_roles as dev_count_distinct_included_roles,
+        prod.count_distinct_sys_user_group_roles as prod_count_distinct_sys_user_group_roles,
+        dev.count_distinct_sys_user_group_roles as dev_count_distinct_sys_user_group_roles
 
     from prod
     full outer join dev
@@ -56,8 +54,8 @@ final as (
 select * 
 from final
 where
-    abs(count_distinct_sys_user_group_ids - count_distinct_sys_user_group_ids) > .1 or
-    abs(count_distinct_sys_user_role_ids - count_distinct_sys_user_role_ids) > .1 or
-    abs(count_distinct_sys_user_role_names - count_distinct_sys_user_role_names) > .1 or
-    abs(count_distinct_included_roles - count_distinct_included_roles) > .1 or
-    abs(count_distinct_sys_user_group_roles - count_distinct_sys_user_group_roles) > .1
+    abs(prod_count_distinct_sys_user_group_ids - dev_count_distinct_sys_user_group_ids) > .1 or
+    abs(prod_count_distinct_sys_user_role_ids - dev_count_distinct_sys_user_role_ids) > .1 or
+    abs(prod_count_distinct_sys_user_role_names - dev_count_distinct_sys_user_role_names) > .1 or
+    abs(prod_count_distinct_included_roles - dev_count_distinct_included_roles) > .1 or
+    abs(prod_count_distinct_sys_user_group_roles - dev_count_distinct_sys_user_group_roles) > .1
